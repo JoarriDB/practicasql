ee17771e5d75286bd7c590c09b94bf6501a5b9b0
Joa-db <joarridb@gmail.com>
2025-12-02 17:08:27 -0500

    Update practica.sql

--- practica.sql
+++ practica.sql
@@ -4,7 +4,7 @@ USE PRACTICA;
 DROP TABLE IF EXISTS cliente;
 CREATE TABLE cliente
  (id                         VARCHAR(3)  NOT NULL,
-  nombre                       VARCHAR(20) NOT NULL,
+  nombre                       VARCHAR(30) NOT NULL,
   telefono                      VARCHAR(20) NOT NULL,
   direccion                    VARCHAR(20),
   ciudad                       VARCHAR(20),
